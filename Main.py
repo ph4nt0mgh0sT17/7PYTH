0bdfe9c48c3c045f90039e5064b479455374004f
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-05 20:40:07 +0100

    Testing python.

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
-print("Test program in 'master' branch");
+print("Test program in 'test-branch' branch")
+
+from Rectangle import Rectangle
+
+'''
+print("\nProgram for calculating the rectangle.")
+a = float(input("Input rectangle's side A: "))
+b = float(input("Input rectangle's side B: "))
+
+rectangle = Rectangle.createRectangle(a,b)
+
+if rectangle is None:
+    print("Your sides are wrong!")
+    exit(0)
+
+print(f"Rectangle's ({a}, {b}) perimeter is: {rectangle.calculatePerimeter()} cm\u00b2.")
+print(f"Rectangle's ({a}, {b}) area is: {rectangle.calculateArea()} cm\u00b2.")
+'''
+
+firstNumber = float(input("First number: "))
+secondNumber = float(input("Second number: "))
+thirdNumber = float(input("Third number: "))
+
+maximumNumber = max(firstNumber,secondNumber,thirdNumber)
+minimumNumber = min(firstNumber,secondNumber,thirdNumber)
+middleNumber = (firstNumber+secondNumber+thirdNumber) - maximumNumber - minimumNumber
+
+print(f"{minimumNumber}, {middleNumber}, {maximumNumber}")
