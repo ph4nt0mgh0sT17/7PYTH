5f3ca5fb981a989b40569e78850886c505e174fe
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-25 21:35:36 +0100

    Added first homework.

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+"""
 print("Test program in 'test-branch' branch")
 
 from Rectangle import Rectangle
@@ -15,11 +16,38 @@ if rectangle is None:
 print(f"Rectangle's ({a}, {b}) perimeter is: {rectangle.calculatePerimeter()} cm\u00b2.")
 print(f"Rectangle's ({a}, {b}) area is: {rectangle.calculateArea()} cm\u00b2.")
 
-firstNumber = float(input("First number: "))
-secondNumber = float(input("Second number: "))
-thirdNumber = float(input("Third number: "))
+"""
 
-maximumNumber = max(firstNumber, secondNumber, thirdNumber)
-minimumNumber = min(firstNumber, secondNumber, thirdNumber)
-middleNumber = (firstNumber + secondNumber + thirdNumber) - maximumNumber - minimumNumber
-print(f"{minimumNumber}, {middleNumber}, {maximumNumber}")
+""" Gets the maximum number """
+def max_number(number1, number2, number3):
+    maximum = number1
+
+    if maximum < number2:
+        maximum = number2
+
+    if maximum < number3:
+        maximum = number3
+
+    return maximum
+
+""" Gets the minimum number """
+def min_number(number1, number2, number3):
+    minimum = number1
+
+    if minimum > number2:
+        minimum = number2
+
+    if minimum > number3:
+        minimum = number3
+
+    return minimum
+
+
+first_number = float(input("First number: "))
+second_number = float(input("Second number: "))
+third_number = float(input("Third number: "))
+
+maximum_number = max_number(first_number, second_number, third_number)
+minimum_number = min_number(first_number, second_number, third_number)
+middle_number = (first_number + second_number + third_number) - maximum_number - minimum_number
+print(f"{minimum_number}, {middle_number}, {maximum_number}")
