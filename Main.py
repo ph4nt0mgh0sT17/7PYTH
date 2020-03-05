1ea7a4ad2ea52f4fc82bf6adaa6c8c983959b2d1
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-03-05 20:07:44 +0100

    Added the third homework.

--- Main.py
+++ Main.py
@@ -1,53 +1,24 @@
-"""
-print("Test program in 'test-branch' branch")
+# Initializing of variables for counting the numbers
+result = 0
+counter = 0
 
-from Rectangle import Rectangle
+# Gets number from the input
+while True:
+    current_number = int(input("Type a number: "))
 
-print("\nProgram for calculating the rectangle.")
-a = float(input("Input rectangle's side A: "))
-b = float(input("Input rectangle's side B: "))
+    # If the number is 0 don't count it and break the while
+    if current_number == 0:
+        break;
 
-rectangle = Rectangle.createRectangle(a, b)
-
-if rectangle is None:
-    print("Your sides are wrong!")
-    exit(0)
-
-print(f"Rectangle's ({a}, {b}) perimeter is: {rectangle.calculatePerimeter()} cm\u00b2.")
-print(f"Rectangle's ({a}, {b}) area is: {rectangle.calculateArea()} cm\u00b2.")
-
-"""
-
-""" Gets the maximum number """
-def max_number(number1, number2, number3):
-    maximum = number1
-
-    if maximum < number2:
-        maximum = number2
-
-    if maximum < number3:
-        maximum = number3
-
-    return maximum
-
-""" Gets the minimum number """
-def min_number(number1, number2, number3):
-    minimum = number1
-
-    if minimum > number2:
-        minimum = number2
-
-    if minimum > number3:
-        minimum = number3
-
-    return minimum
+    result += current_number
+    counter += 1
 
 
-first_number = float(input("First number: "))
-second_number = float(input("Second number: "))
-third_number = float(input("Third number: "))
+sum = result
 
-maximum_number = max_number(first_number, second_number, third_number)
-minimum_number = min_number(first_number, second_number, third_number)
-middle_number = (first_number + second_number + third_number) - maximum_number - minimum_number
-print(f"{minimum_number}, {middle_number}, {maximum_number}")
+# The average is calculated only if the counter is bigger than zero -> Division by zero is forbidden
+if counter > 0:
+    average = result / counter
+
+print(sum)
+print(round(average, 2))
