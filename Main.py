1f5e6b3b8420761a79b36de9480f33ab0557a50f
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-03-12 19:09:46 +0100

    Added the fourth homework.

--- Main.py
+++ Main.py
@@ -1,53 +1,31 @@
-"""
-print("Test program in 'test-branch' branch")
+import turtle
+import random
 
-from Rectangle import Rectangle
+# Window of the turtle
+window = turtle.Screen()
 
-print("\nProgram for calculating the rectangle.")
-a = float(input("Input rectangle's side A: "))
-b = float(input("Input rectangle's side B: "))
+# Background color of the window
+window.bgcolor('white')
 
-rectangle = Rectangle.createRectangle(a, b)
-
-if rectangle is None:
-    print("Your sides are wrong!")
-    exit(0)
-
-print(f"Rectangle's ({a}, {b}) perimeter is: {rectangle.calculatePerimeter()} cm\u00b2.")
-print(f"Rectangle's ({a}, {b}) area is: {rectangle.calculateArea()} cm\u00b2.")
-
-"""
-
-""" Gets the maximum number """
-def max_number(number1, number2, number3):
-    maximum = number1
-
-    if maximum < number2:
-        maximum = number2
-
-    if maximum < number3:
-        maximum = number3
-
-    return maximum
-
-""" Gets the minimum number """
-def min_number(number1, number2, number3):
-    minimum = number1
-
-    if minimum > number2:
-        minimum = number2
-
-    if minimum > number3:
-        minimum = number3
-
-    return minimum
+# Creting the turtle
+t = turtle.Turtle()
+t.speed(40)
 
 
-first_number = float(input("First number: "))
-second_number = float(input("Second number: "))
-third_number = float(input("Third number: "))
+forward_speed = 200
+angle_change = 90
 
-maximum_number = max_number(first_number, second_number, third_number)
-minimum_number = min_number(first_number, second_number, third_number)
-middle_number = (first_number + second_number + third_number) - maximum_number - minimum_number
-print(f"{minimum_number}, {middle_number}, {maximum_number}")
+t.fillcolor('blue')
+
+t.begin_fill()
+
+for i in range(150):
+    for i in range(4):
+        t.forward(forward_speed)
+        t.left(angle_change)
+
+    t.left(angle_change -random.randint(0, 200))
+
+t.end_fill()
+
+window.exitonclick()
